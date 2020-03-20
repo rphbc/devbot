332b508e40c120454ddc9b7d2f39181a5becb280
Raphael Costa <raphaelbc@gmail.com>
2020-03-20 13:29:17 -0300

    updating requirements and add regex on bot good morning message

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 # bot.py
-import os
+
+import random
+import re
 
 import discord
-import random
 from decouple import config
 
-# load_dotenv()
 TOKEN = config('TOKEN')
 
 client = discord.Client()
 
+
+# channel id is 690226588696051796
 
 @client.event
 async def on_ready():
@@ -26,9 +28,9 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
+    author = message.author
     channel = message.channel
-    message = message.content.lower()
-    print(message)
+    content = message.content.lower()
     brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
         'Bingpot!',
@@ -38,16 +40,16 @@ async def on_message(message):
         ),
     ]
 
-    if message == '99!':
+    if content == '99!':
         response = random.choice(brooklyn_99_quotes)
         await channel.send(response)
-    if message == 'Oi Bot'.lower():
-        response = f'Olá {message.author.display_name}, seja bem-vindo(a)!'
+    if content == 'Oi Bot'.lower():
+        response = f'Olá {author.display_name}, seja bem-vindo(a)!'
         await channel.send(response)
-    if message == 'Bom dia Bot'.lower():
+    if re.search(r'^(bom\s?dia.*?bot)', content):
         response = f'Bom dia mano.'
         await channel.send(response)
-    if message == 'vai tomar no cu bot'.lower():
+    if content == 'vai tomar no cu bot'.lower():
         response = f'Toma no cú é vitamina, como tu e tuas prima'
         await channel.send(response)
 
