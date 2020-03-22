382f531a88d5f10e963669640055897cd00669d9
Rafael Mendes <rafael.mendes@birmind.com.br>
2020-03-22 18:59:50 -0300

    DevBot Command class is Automatically appended to the routes when execute_command is called

--- devbot/core/runner.py
+++ devbot/core/runner.py
@@ -1,4 +1,15 @@
+from devbot.core.commands import DevBot
+from devbot.core.routers import Route
+
+
+def append_devbot_class(routes):
+    routes.append(
+        Route('devbot', DevBot, class_args=(routes, ))
+    )
+
+
 async def execute_command(routes, message):
+    append_devbot_class(routes)
     for route in routes:
         command = route.check_route(message.content)
         if command:
