d0f9977943c5facb523925a1724697dc9bf4d157
Rafael Mendes <rafael.mendes@birmind.com.br>
2020-03-22 19:00:28 -0300

    Added decostring to GreetCommand class

--- devbot/commands/greetings.py
+++ devbot/commands/greetings.py
@@ -2,11 +2,20 @@ from devbot.core.commands import BaseCommand
 
 
 class GreetCommand(BaseCommand):
+    """
+    Greets People
+    """
     actions = {
         'say_hi': {'arguments': {'to': {'dtype': 'str'}}}
     }
 
     async def say_hi(self, to):
+        """
+        Says hi to the person passed as the "to" argument.
+        If value me is passed, it greets your name instead.
+        :param to: str
+        :return: str
+        """
         name = to
         if to == 'me':
             name = self._message.author.name
