00302fbec0e0b101f1a5ca820e3803d6ef5f44e0
Rafael Mendes <rafael.mendes@birmind.com.br>
2020-03-22 18:57:20 -0300

    Created DevBot Command class to handle all commands management specific actions

--- devbot/core/commands.py
+++ devbot/core/commands.py
@@ -9,8 +9,36 @@ dtype_regex = {
 class BaseCommand:
     def __init__(self, message):
         self._message = message
+        if 'help' not in self.actions:
+            self.actions['help'] = {'arguments': {}}
 
     actions = {}
+
+    @classmethod
+    def describe(cls):
+        class_doc = cls.describe_class() or ''
+        actions_doc = cls.describe_actions() or ''
+        route_data = f'{class_doc}'
+        route_data += '\n-> Actions:\n'
+        route_data += actions_doc
+        return route_data
+
+    @classmethod
+    def describe_class(cls):
+        return cls.__doc__
+
+    @classmethod
+    def describe_actions(cls):
+        actions = []
+        for action, action_data in cls.actions.items():
+            action_method_doc = getattr(cls, action).__doc__
+            action_description = f'.{action}:'
+            if action_method_doc:
+                if action_method_doc[0] != '\n':
+                    action_method_doc = f'\n{action_method_doc}'
+                action_description += f'{action_method_doc}'
+            actions.append(action_description)
+        return '\n'.join(actions)
 
     async def _create_arguments_regex(self, arguments):
         regex = r''
@@ -56,9 +84,36 @@ class BaseCommand:
             return {}
         return match.groupdict()
 
+    async def help(self):
+        return self.describe()
+
     async def run_command(self, command_text):
         action, action_content  = await self._search_actions(command_text)
         if not action:
             return False
         action_args = await self._get_action_arguments(action, action_content)
         return await getattr(self, action)(**action_args)
+
+
+class DevBot(BaseCommand):
+    """
+    DevBot management commands.
+    """
+    def __init__(self, message, routes):
+        super(DevBot, self).__init__(message)
+        self._routes = routes
+
+    actions = {'list_routes': {'arguments': {}}}
+
+    async def list_routes(self):
+        """
+        List all existent command routes.
+        :return: str
+        """
+        available_routes = []
+        for route in self._routes:
+            route_data = f'{route.route_indicator}{route.route_name}:\n'
+            class_doc = route.command_class.describe_class() or ''
+            route_data += class_doc
+            available_routes.append(route_data)
+        return '\n-----------------\n'.join(available_routes)
